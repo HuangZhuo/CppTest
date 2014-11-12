6482bf15214c874b250cdf7adc8b46e02e85c881
HuangZhuo <idorable@163.com>
2014-11-12 14:45:37 +0800

    修改main.cpp文件
    添加描述。

--- CppTest/main.cpp
+++ CppTest/main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+
+/*
+每一项测试都有main函数出现在对应测试文件中。
+通过一个【是否注释】的TEST_×××宏作为【开关】决定哪一个main函数运行。
+*/
+
+//#define TEST_
+
+#ifdef TEST_
 void main()
 {
+	std::cout << "Hello World!" << "\n";
 	return;
 }
+#endif
